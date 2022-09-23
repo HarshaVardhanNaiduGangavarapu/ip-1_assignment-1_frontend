53778996bbc9b483b27db217f9fe0afcf6814cb0
Gopikrishnan Rajeev <gopikrishnan.rmg@live.in>
2022-09-22 22:24:10 -0400

    Added basic login screen

--- authentica/src/components/CardView.jsx
+++ authentica/src/components/CardView.jsx
@@ -1,20 +1,33 @@
 import React from 'react'
 import '../styles/CardView.css';
+import { MdEmail } from "react-icons/md";
+import { MdPassword } from "react-icons/md";
+import { FaUserAlt } from 'react-icons/fa';
+import google from '../assets/icons8-google-48.svg'
 
 function CardView() {
   return (
     <div className='form'>
     <form className='contents'>
+      <FaUserAlt className='user'/>
       <header className='header'>Login</header>
       <div className='input-container'>
+        <MdEmail style={{color:"#009B77"}}/>
         <input className='input' type='text' placeholder='Enter your email' name='uname' required />
       </div>
       <div className='input-container'>
+      <MdPassword style={{color:"#009B77"}}/>
         <input className='input' type='password' placeholder='Password' name='pass' required />
       </div>
       <div className='button-container'>
-        <input type='submit' />
+        <input className='button' type='submit' />
       </div>
+      <p className='login'>Or login with</p>
+      <img src={google} alt="loading..." />
+      <br></br>
+      <div class="line-1"></div>
+      <a className='register' href=''>Register</a>
+      
     </form>
   </div>
   )
